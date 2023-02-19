9252bc4d921bc7609f2c0bd980321863c01b9962
jamesjavanyc` <1931736857@qq.com>
2023-02-19 15:21:52 -0500

    lesson4

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key")
+        res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     })
     .use('/', require('./routes'));
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ router.use('/contacts', require('./contacts'))
 
 router.use("/", require("./name-route"))
 
+router.use('/api-docs', require('./swagger'));
+
 module.exports = router;
