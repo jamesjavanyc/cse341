7de944fc2417230f5e57a0d4d9007b56077831d1
jamesjavanyc` <1931736857@qq.com>
2023-02-19 15:33:10 -0500

    cors headers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key")
-        res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     })
     .use('/', require('./routes'));
