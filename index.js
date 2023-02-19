ec67b0f0a18f20c371dae18af2e29992d0c0b417
jamesjavanyc` <1931736857@qq.com>
2023-02-19 15:39:37 -0500

    lesson4

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
+var cors = require('cors')
 const port = process.env.SERVER_PORT || 5000;
 const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
 const app = express()
+app.use(cors())
 
 app
     .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key")
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
-    })
+    .use(cors())
     .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
